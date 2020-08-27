5567cbb8c520f2f7b4d8e527899d89ba540f8651
enriqueizel2 <eleaoizel@gmail.com>
2020-08-27 19:40:14 -0400

    feat(components/HistoricoList: adicionando area tocavel
    Envolvendo container de de itens para que ele tenha uma acao de LongPress

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,23 +1,26 @@
 import React from "react";
 import Icon from "@expo/vector-icons/Feather";
+import { TouchableWithoutFeedback } from "react-native";
 import { Container, Tipo, IconView, TipoText, ValorText } from "./styles";
 
-const HistoricoList = ({ data }) => {
+const HistoricoList = ({ data, deleteItem }) => {
   return (
-    <Container>
-      <Tipo>
-        <IconView tipo={data.tipo}>
-          <Icon
-            name={data.tipo === "despesa" ? "arrow-down" : "arrow-up"}
-            color="#fff"
-            size={20}
-          />
-          <TipoText>{data.tipo}</TipoText>
-        </IconView>
-      </Tipo>
+    <TouchableWithoutFeedback onLongPress={() => deleteItem(data)}>
+      <Container>
+        <Tipo>
+          <IconView tipo={data.tipo}>
+            <Icon
+              name={data.tipo === "despesa" ? "arrow-down" : "arrow-up"}
+              color="#fff"
+              size={20}
+            />
+            <TipoText>{data.tipo}</TipoText>
+          </IconView>
+        </Tipo>
 
-      <ValorText>R$ {data.valor}</ValorText>
-    </Container>
+        <ValorText>R$ {data.valor}</ValorText>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 };
 
