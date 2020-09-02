ad385c06701837d85cce1c01a8e92c1dd1ee6c16
enriqueizel2 <eleaoizel@gmail.com>
2020-09-02 10:34:58 -0400

    feat(components/CustomDrawer): criando estilos dos components

---
+++ src/components/CustomDrawer/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  align-items: center;
+  justify-content: center;
+  margin-top: 25px;
+`;
+
+export const Logo = styled.Image`
+  width: 85px;
+  height: 85px;
+`;
+
+export const WelcomeText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  margin-top: 5px;
+`;
+
+export const NameText = styled.Text`
+  color: #fff;
+  font-size: 17px;
+  font-weight: bold;
+  padding-bottom: 25px;
+`;
